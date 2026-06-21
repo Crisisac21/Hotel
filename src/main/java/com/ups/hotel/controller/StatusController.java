09d29935b2e4270599c8c155b14bb7c1524a7831
Crisisac21 <cristian.murillo369@outlook.com>
2026-06-21 16:38:51 -0500

    Allow CORS for status endpoint

--- src/main/java/com/ups/hotel/controller/StatusController.java
+++ src/main/java/com/ups/hotel/controller/StatusController.java
@@ -1,5 +1,6 @@
 package com.ups.hotel.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.Instant;
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class StatusController {
