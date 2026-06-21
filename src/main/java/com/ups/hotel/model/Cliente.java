4dc6d335d78988e819d66ed52d7cded0832d0cbf
Crisisac21 <cristian.murillo369@outlook.com>
2026-06-21 16:38:33 -0500

    Add validation constraints to Cliente model

--- src/main/java/com/ups/hotel/model/Cliente.java
+++ src/main/java/com/ups/hotel/model/Cliente.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "clientes")
@@ -12,8 +15,17 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 120)
     private String nombre;
+
+    @NotBlank
+    @Size(max = 32)
     private String telefono;
+
+    @NotBlank
+    @Email
     private String email;
 
     public Cliente() {
