c1b3607005b9f1a81fe7bbc1132093f8f092bdd7
Crisisac21 <cristian.murillo369@outlook.com>
2026-06-28 17:48:29 -0500

    Configurar conexión a MySQL y propiedades de la aplicación

--- src/main/java/com/ups/hotel/config/SwaggerConfig.java
+++ src/main/java/com/ups/hotel/config/SwaggerConfig.java
@@ -1,6 +1,12 @@
 package com.ups.hotel.config;
 
+<<<<<<< HEAD
 import org.springdoc.core.GroupedOpenApi;
+=======
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+>>>>>>> 660b226 (Add OpenAPI metadata and grouped API docs)
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +18,14 @@ public class SwaggerConfig {
                 .group("hotel-reservas")
                 .pathsToMatch("/api/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI hotelOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Hotel Reservas API")
+                        .description("API for hotel reservation management")
+                        .version("1.0.0"));
     }
 }
